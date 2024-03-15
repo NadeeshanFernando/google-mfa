de9c6b239325148ca1d46d6fea21e29aa0eef72c
NadeeshanFernando <52916954+NadeeshanFernando@users.noreply.github.com>
2024-03-15 23:59:14 +0530

    code modified

--- src/main/java/com/anton/mfa/controller/UserController.java
+++ src/main/java/com/anton/mfa/controller/UserController.java
@@ -6,15 +6,13 @@ import com.anton.mfa.model.User;
 import com.anton.mfa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author by nadeeshan_fdz
  */
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
